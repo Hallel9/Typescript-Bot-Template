1b32fec24fc91fe9389c140eb589c7f27d703a43
Hallel Shapiro <hallelshapiro@gmail.com>
2022-01-08 18:44:35 -0500

    Added client owners

--- src/Structures/Client.ts
+++ src/Structures/Client.ts
@@ -10,6 +10,7 @@ import mongoose from 'mongoose'
 config()
 export class ExtendedClient extends Discord.Client {
     commands: Discord.Collection<string, CommandType> = new Discord.Collection()
+    owners = []
     constructor(options: Discord.ClientOptions) {
         super(options)
     }
